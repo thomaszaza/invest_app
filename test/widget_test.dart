d582bed1fab53598257bd79982940f193f7276c2
thomaszaza <thomaszaza@users.noreply.github.com>
2026-08-19 15:24:21 +0000

    deploy: 7315ca084a7bf1f867ae5f7af638a2e873ea2cf8

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Test basique vide', (WidgetTester tester) async {
-    // Un test qui valide simplement que vrai = vrai
-    expect(true, true);
-  });
-}
